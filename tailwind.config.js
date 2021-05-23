c8eb63d348b76112ac4a0b687f3fa5ae258741da
Edmund Hung <me@edmund.dev>
2021-05-23 21:13:02 +0200

    feat: Setup Tailwind and Postcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    "./app/**/*.tsx",
+    "./app/**/*.jsx",
+    "./app/**/*.js",
+    "./app/**/*.ts"
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+};
