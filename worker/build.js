44701abb844c92597a60f85a3e697f4a808dab2a
Edmund Hung <me@edmund.dev>
2021-08-22 20:53:30 +0200

    build: simplify worker build step

--- worker/build.js
+++
@@ -1,9 +0,0 @@
-const esbuild = require("esbuild");
-
-esbuild.build({
-  entryPoints: ["worker/index.ts"],
-  bundle: true,
-  format: "esm",
-  define: { "process.env.NODE_ENV": '"production"' },
-  outfile: "./worker.js",
-});
