bd2b7ba0f0f7ae95d01960f8f2312d7d5aa23f57
Edmund Hung <me@edmund.dev>
2021-09-08 18:25:19 +0200

    feat: add cache-control header for blog post

--- app/routes/blog.$slug.tsx
+++ app/routes/blog.$slug.tsx
@@ -1,6 +1,12 @@
-import type { MetaFunction, LinksFunction, LoaderFunction } from "remix";
+import type { MetaFunction, HeadersFunction, LinksFunction, LoaderFunction } from "remix";
 import { useRouteData, json } from "remix";
 import { parse } from '../markdown.server';
+
+export let headers: HeadersFunction = ({ loaderHeaders }) => {
+  return {
+    'Cache-Control': loaderHeaders.get('Cache-Control'),
+  };
+};
 
 export let meta: MetaFunction = ({ data }) => {
   return {
@@ -10,15 +16,17 @@ export let meta: MetaFunction = ({ data }) => {
 
 export let loader: LoaderFunction = async ({ params, context }) => {
   const [content, metadata] = await context.getContent('blog', params.slug);
-
-  if (content === null) {
-    return json({}, 404);
-  }
-
-  return {
-    content: await parse(content),
+  const data = {
+    content: content ? await parse(content) : null,
     metadata,
   };
+
+  return json(data, {
+    status: content !== null ? 200 : 404,
+    headers: {
+      'Cache-Control': 'max-age=3600',
+    },
+  });
 };
 
 export default function BlogSlug() {
