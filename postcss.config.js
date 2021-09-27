991cec98894fcc492905688ba6698f1c2200e799
Edmund Hung <me@edmund.dev>
2021-09-27 23:14:26 +0200

    chore: setup css minify with cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
-module.exports = {
+module.exports = ({ env }) => ({
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    cssnano: env === 'production' ? { preset: 'default' } : false,
   },
-};
+});
