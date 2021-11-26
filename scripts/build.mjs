0b9497f95139f0fe82496fc355ffba23c75e6c2f
Edmund Hung <me@edmund.dev>
2021-11-26 20:51:32 +0100

    chore: update build script

---
+++ scripts/build.mjs
@@ -0,0 +1,33 @@
+import * as esbuild from 'esbuild';
+
+async function build() {
+  const mode = process.env.NODE_ENV?.toLowerCase() ?? 'development';
+  const version = process.env.VERSION ?? new Date().toISOString();
+
+  console.log(`Building Worker in ${mode} mode (version: ${version})`);
+
+  const outfile = './dist/worker.mjs';
+  const startTime = Date.now();
+  const result = await esbuild.build({
+    entryPoints: ['./worker/index.ts'],
+    bundle: true,
+    minify: mode === 'production',
+    sourcemap: mode !== 'production',
+    format: 'esm',
+    metafile: true,
+    define: {
+      'process.env.NODE_ENV': `"${mode}"`,
+      'process.env.VERSION': `"${version}"`,
+    },
+    outfile,
+  });
+  const endTime = Date.now();
+
+  console.log(`Built in ${endTime - startTime}ms`);
+
+  if (mode === 'production') {
+    console.log(await esbuild.analyzeMetafile(result.metafile));
+  }
+}
+
+build().catch(e => console.error('Unknown error caught during build:', e));
